4035dd73a0ed875712e58997010ae72a808c5fb4
Merojiddin Sirojiddinov <sirojiddinovmerojiddin@gmail.com>
2024-08-10 18:41:05 +0500

    git init and fix first problem

---
+++ oddEvenChecker/script.js
@@ -0,0 +1,13 @@
+const oddEvenCheker = (n) => {
+    if(n % 2 == 0) return "This is Even number";
+    else return "This is Odd number";
+}
+
+console.log(oddEvenCheker(5));
+
+// ChatGPT's answer
+/*
+const oddEvenCheker = (n) => n % 2 == 0 ? "This is Even number" : "This is Odd number";
+
+console.log(oddEvenCheker(5));
+*/
