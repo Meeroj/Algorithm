0c18da139c2b29295e4118d79a285aafcc10fa05
Merojiddin Sirojiddinov <sirojiddinovmerojiddin@gmail.com>
2024-08-11 12:41:49 +0500

    solved summ of array problem

---
+++ junior/level1/summOfArray/script.js
@@ -0,0 +1,13 @@
+const summOfArray = (arr) => {
+    return arr.reduce((a, b) => a + b)
+}
+
+console.log(summOfArray([1, 2, 3, 7]))
+
+// chatGPT'a answer
+/*
+const summOfArray = arr => arr.reduce((a, b) => a + b);
+
+console.log(summOfArray([1, 2, 3, 7]));
+
+*/
