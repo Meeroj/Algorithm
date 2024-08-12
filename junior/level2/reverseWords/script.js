087b6e09be920ad0ec73eef5f2c9effe870ab83d
Merojiddin Sirojiddinov <sirojiddinovmerojiddin@gmail.com>
2024-08-12 21:54:13 +0500

    solved reverse words in text problem

---
+++ junior/level2/reverseWords/script.js
@@ -0,0 +1,10 @@
+const reverseWord = text => {
+    const splitText = text.split(' ')
+    const reverseWordsTextList = []
+    for (let i = 0; i < splitText.length; i++) {
+        reverseWordsTextList.push(splitText[i].split('').reverse('').join(''))
+    }
+
+    return reverseWordsTextList.join(' ')
+}
+console.log(reverseWord('Salom mening ismim nimaligi ni bilsanmi?'))
