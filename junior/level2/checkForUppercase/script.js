345a3de8b5f0b0fbb4463a18084271e2544d99d8
Merojiddin Sirojiddinov <sirojiddinovmerojiddin@gmail.com>
2024-08-17 15:37:18 +0500

    added check Uppercase

--- junior/level2/checkForUppercase/script.js
+++ junior/level2/checkForUppercase/script.js
@@ -1,6 +1,12 @@
 const CheckUppercase = text => {
     const splitText = text.split('')
     for(let i in splitText){
-        if(splitText[i] === splitText[i].uppercase)
+        if(splitText[i].charCodeAt() === splitText[i].toUpperCase().charCodeAt()){
+            return true
+        }
+
     }
+    return false
 }
+
+console.log(CheckUppercase('ahdbabiysbhd Aiasujdbuoasjb'))
